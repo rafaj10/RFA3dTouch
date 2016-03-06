b86bac3da19214b8debd62eb7f578442c76a7a95
Rafael Assis <rafazassis@gmail.com>
2016-03-06 09:42:18 -0300

    Initial Commit

---
+++ RFA3dTouch/RFA3dTouch.h
@@ -0,0 +1,19 @@
+//
+//  RFA3dTouch.h
+//  RFA3dTouch
+//
+//  Created by Rafael Assis on 3/6/16.
+//  Copyright © 2016 rfa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RFA3dTouch.
+FOUNDATION_EXPORT double RFA3dTouchVersionNumber;
+
+//! Project version string for RFA3dTouch.
+FOUNDATION_EXPORT const unsigned char RFA3dTouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RFA3dTouch/PublicHeader.h>
+
+
